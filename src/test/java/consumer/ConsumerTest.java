35f0bb6f38d72c313c983032cdb9c699dab30112
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2024-04-25 00:51:33 +0530

    Refactor ConsumerTest to enable pact generation with Regex using MessageInteractionBuilder

--- src/test/java/consumer/ConsumerTest.java
+++ src/test/java/consumer/ConsumerTest.java
@@ -2,17 +2,19 @@ package consumer;
 
 import au.com.dius.pact.consumer.dsl.PactBuilder;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.junit5.PactConsumerTest;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.consumer.junit5.ProviderType;
-import au.com.dius.pact.core.model.V4Interaction.AsynchronousMessage;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import au.com.dius.pact.core.model.V4Interaction;
 import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -20,9 +22,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(PactConsumerTestExt.class)
+@PactConsumerTest
 @PactTestFor(
     providerName = "kafka-message-provider",
-    providerType = ProviderType.ASYNCH)
+    providerType = ProviderType.ASYNCH,
+    pactVersion = PactSpecVersion.V4)
 public class ConsumerTest {
 
   @BeforeAll
@@ -31,33 +35,52 @@ public class ConsumerTest {
   }
 
   @Pact(consumer = "kafka-message-consumer")
-  public V4Pact messageFromKafkaProvider(PactBuilder builder) {
+  public V4Pact createPact(PactBuilder builder) {
     PactDslJsonBody jsonBody = new PactDslJsonBody();
 
     Objects.requireNonNull(
-            Objects.requireNonNull(jsonBody.uuid("id", "556732b9-ed6c-40dd-81b2-dbd34eaeae0e")
+            Objects.requireNonNull(jsonBody
+                    .stringMatcher("id", "^[a-zA-Z0-9-]*$", "24456732b9-ed6c-40dd-81b2-dggdhdhd12455")
                     .object("data")
                     .object("payload")
-                    .stringValue("name", "Thoughtworks")
+                    .stringMatcher("name", "^Krish[0-9]*$", "Krish1001")
                     .object("employeeDetail")
-                    .stringType("id", "kc:employee:31833")
+                    .stringType("id", "kc:employee:12345")
                     .closeObject())
                 .closeObject())
         .closeObject();
 
+    /**
+     * Without using MessageInteractionBuilder
+     * Using this approach with PactSpecVersion.V4 generates a pact without regex
+     */
+//    return builder
+//        .expectsToReceive("A sample message", "core/interaction/message")
+//        .with(Map.of("message.contents", jsonBody))
+//        .toPact();
+
+    /**
+     * With MessageInteractionBuilder
+     * Using this approach with PactSpecVersion.V4 generates a pact with regex
+     */
     return builder
-        .expectsToReceive("A sample message", "core/interaction/message")
-        .with(Map.of("message.contents", jsonBody))
+        .expectsToReceiveMessageInteraction(
+            "A sample message", messageInteractionBuilder -> {
+              return messageInteractionBuilder.withContents(messageContentsBuilder -> {
+                return messageContentsBuilder
+                    .withContent(jsonBody)
+                    .withMetadata(new HashMap<>());
+              });
+            })
         .toPact();
+
   }
 
   @Test
-  @PactTestFor(pactMethod = "messageFromKafkaProvider")
-  void testMessageFromProvider(List<AsynchronousMessage> messages)
+  @PactTestFor(pactMethod = "createPact")
+  void testMessageFromProvider(List<V4Interaction.AsynchronousMessage> messages)
       throws JsonProcessingException {
-    JsonNode msg = new ObjectMapper().readValue(messages.get(0).contentsAsString(),
-        JsonNode.class);
-
+    JsonNode msg = new ObjectMapper().readValue(messages.get(0).contentsAsString(), JsonNode.class);
     Assertions.assertNotNull(msg);
   }
 
